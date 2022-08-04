e4f6b31ab570ccbf7bda2a723e3e8c8d748822c8
RunzeZ0710 <82784355+RunzeZ0710@users.noreply.github.com>
2022-08-04 11:34:23 -0400

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,18 +14,19 @@ function Navbar(){
                 <Link to="/matches" className="nav-links">
                         Matches
                 </Link>
+                <Link to="calendar" className="nav-links">
+                    Calendar
+                </Link>
                 <div className="dropdown">
                     <button className="nav-links" onClick={handleClick}>
                         Our Teams
                     </button>
-                    <div className="dropdown-content">
+                    <div className={click ? "dropdown-content active" : "dropdown-content"}>
                         <Link to="/boysTeam" className="subnav-links"> Boys Team</Link>
                         <Link to="/girlsTeam" className="subnav-links"> Girls Team</Link>
                     </div>
                 </div>
-                <Link to="calendar" className="nav-links">
-                    Calendar
-                </Link>
+                
         
             </div>
         </>
