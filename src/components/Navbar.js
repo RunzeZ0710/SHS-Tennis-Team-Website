90f0ea42c0c4cb62a3fe041e5d1552a2ddf339f8
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-05 14:44:04 -0400

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,23 @@ function Navbar(){
         <>
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    SHS Tennis
+                    [LOGO]
                 </Link>  
-                <Link to="/matches" className="nav-links">
-                        Matches
-                </Link>
-                <Link to="calendar" className="nav-links">
-                    Calendar
-                </Link>
-                <div className="dropdown">
-                    <button className="nav-links" onClick={handleClick}>
-                        Our Teams
-                    </button>
-                    <div className={click ? "dropdown-content active" : "dropdown-content"}>
-                        <Link to="/boysTeam" className="subnav-links"> Boys Team</Link>
-                        <Link to="/girlsTeam" className="subnav-links"> Girls Team</Link>
+                <div className="nav-links-container">
+                    <Link to="/matches" className="nav-links">
+                            Matches
+                    </Link>
+                    <div className="dropdown">
+                        <button className="nav-links" onClick={handleClick}>Our Teams</button>
+                        <div className={click ? "dropdown-content active" : "dropdown-content"}>
+                            <Link to="/boysTeam" className="subnav-links"> Boys Team</Link>
+                            <Link to="/girlsTeam" className="subnav-links"> Girls Team</Link>
+                        </div>
                     </div>
+                    <Link to="calendar" className="nav-links">
+                        Calendar
+                    </Link>
                 </div>
-                
-        
             </div>
         </>
     );
