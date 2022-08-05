54acb251746242e20e7fa2cf7cb106c0170e7453
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-05 12:57:42 -0400

    Girls team Photo stuff

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../components/Body.css";
+import girlsTeam from "./photos/girlsTeam.png"
+
 
 function Body() {
     return(
         <>
         <div className="body">
-            <div className="header">
-                <h1>Shrewsbury - Westborough</h1>
-            </div>
-            <div className="mainBody">
-                <p>Deserunt culpa anim duis eu pariatur ad est duis enim. Aliqua labore ut nulla ea occaecat. Quis officia sunt ut id commodo ex incididunt in ad et. Nisi ex exercitation eu enim do eu ut pariatur consequat eiusmod commodo proident minim do. Laboris exercitation amet dolor occaecat duis. Incididunt dolore nulla consequat excepteur proident nulla do elit. Mollit qui eu aute nostrud nostrud aliqua laborum voluptate non officia amet id.</p>
+            <img className="girls-team-photo" src={girlsTeam} alt="Girls Team" />
+            <div className="header-wrapper">
+                <h1 className="header">SHS Tennis</h1>
             </div>
         </div>
         </>
