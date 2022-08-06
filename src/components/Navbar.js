362329b0aa9715e84dc61bbba8f7db0892ac61f2
RunzeZ0710 <82784355+RunzeZ0710@users.noreply.github.com>
2022-08-05 22:33:50 -0400

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,25 @@ import {Link} from "react-router-dom";
 function Navbar(){
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
+    const closeDrop = () => setClick(false);
     return(
         <>
         <nav className="navbar">
             <div className="navbar-container"> 
-                <Link to="/" className="nav-links">
+                <Link to="/" onClick={closeDrop} className="nav-links">
                     Home
                 </Link> 
-                <Link to="/matches" className="nav-links">
+                <Link to="/matches" onClick={closeDrop} className="nav-links">
                     Matches
                 </Link>
                 <div className="dropdown">
                     <button onClick={handleClick}>Our Teams</button>
                     <div className={click ? "dropdown-content active" : "dropdown-content"}>
-                        <Link to="/boysTeam" className="subnav-links"> Boys Team</Link>
-                        <Link to="/girlsTeam" className="subnav-links"> Girls Team</Link>
+                        <Link to="/boysTeam" onClick={closeDrop} className="subnav-links"> Boys Team</Link>
+                        <Link to="/girlsTeam" onClick={closeDrop} className="subnav-links"> Girls Team</Link>
                     </div>
                 </div>
-                <Link to="calendar" className="nav-links">
+                <Link to="calendar" onClick={closeDrop} className="nav-links">
                     Calendar
                 </Link>
             </div>
