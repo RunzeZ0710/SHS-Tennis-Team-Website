3900e807a8edaa678f1598a42d8eee0144ea9e62
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-06 15:59:39 -0400

    hopefully fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,29 +5,28 @@ import {Link} from "react-router-dom";
 function Navbar(){
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
-    const closeDrop = () => setClick(false);
     return(
         <>
-        <nav className="navbar">
-            <div className="navbar-container"> 
-                <Link to="/" onClick={closeDrop} className="nav-links">
-                    Home
-                </Link> 
-                <Link to="/matches" onClick={closeDrop} className="nav-links">
-                    Matches
-                </Link>
-                <div className="dropdown">
-                    <button onClick={handleClick}>Our Teams</button>
-                    <div className={click ? "dropdown-content active" : "dropdown-content"}>
-                        <Link to="/boysTeam" onClick={closeDrop} className="subnav-links"> Boys Team</Link>
-                        <Link to="/girlsTeam" onClick={closeDrop} className="subnav-links"> Girls Team</Link>
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    [LOGO]
+                </Link>  
+                <div className="nav-links-container">
+                    <Link to="/matches" className="nav-links">
+                            Matches
+                    </Link>
+                    <div className="dropdown">
+                        <button className="nav-links" onClick={handleClick}>Our Teams</button>
+                        <div className={click ? "dropdown-content active" : "dropdown-content"}>
+                            <Link to="/boysTeam" className="subnav-links"> Boys Team</Link>
+                            <Link to="/girlsTeam" className="subnav-links"> Girls Team</Link>
+                        </div>
                     </div>
+                    <Link to="calendar" className="nav-links">
+                        Calendar
+                    </Link>
                 </div>
-                <Link to="calendar" onClick={closeDrop} className="nav-links">
-                    Calendar
-                </Link>
             </div>
-        </nav>
         </>
     );
 }
