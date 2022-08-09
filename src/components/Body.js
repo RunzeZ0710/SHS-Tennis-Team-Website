740bfd667776cc1582bb8f4a3fe85715e8e84cb1
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-09 15:38:52 -0400

    Core Values

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "../components/Body.css";
-import girlsTeam from "./photos/girlsTeam.jpg"
 
 
 function Body() {
     return(
         <>
         <div className="body">
+            <div className="core-values">
+                <div className="core-value">
+                    <h2>Perserverance</h2>
+                    <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
+                </div>
+                <div className="core-value">
+                    <h2>Reponsibility</h2>
+                    <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
+                </div>
+                <div className="core-value">
+                    <h2>Teamwork</h2>
+                    <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
+                </div>
+            </div>
         </div>
         </>
     )
