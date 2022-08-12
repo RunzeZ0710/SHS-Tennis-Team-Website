48124a30363237a2945a0ff470f6acee43ae58fc
RunzeZ0710 <82784355+RunzeZ0710@users.noreply.github.com>
2022-08-12 16:01:28 -0400

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../components/Body.css";
+import perserverance from "../components/graphics/perserverance.png"
+import responsibility from "../components/graphics/responsibility.png"
+import teamwork from "../components/graphics/teamwork.png"
 
 
 function Body() {
@@ -8,14 +11,17 @@ function Body() {
         <div className="body">
             <div className="core-values">
                 <div className="core-value">
+                    <img src={perserverance} alt="perserverance" height="250px" width="250px" />
                     <h2>Perserverance</h2>
                     <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
                 </div>
                 <div className="core-value">
+                    <img src={responsibility} alt="responsibility" height="250px" width="250px" />
                     <h2>Reponsibility</h2>
                     <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
                 </div>
                 <div className="core-value">
+                    <img src={teamwork} alt="teamwork" height="250px" width="250px" />
                     <h2>Teamwork</h2>
                     <p>Qui pariatur nostrud est anim esse adipisicing enim nisi fugiat. Sint aute sint deserunt sint anim duis. Magna ad Lorem sit quis sint laborum. Eiusmod duis eiusmod eu excepteur exercitation officia ad velit tempor in eu.</p>
                 </div>
