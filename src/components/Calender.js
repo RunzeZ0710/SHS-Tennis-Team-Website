7c84502fc8cff048bc4567b349c89484ef29aa41
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-26 21:48:05 -0400

    changes

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -3,7 +3,7 @@ import "../components/Calendar.css"
 
 function Body(props) {
     const {
-        iframeSource = '<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23E4C441&ctz=America%2FNew_York&showPrint=0&title=Tennis%20Team%20Schedule&showTabs=0&showTz=1&showTitle=0&showNav=1&showDate=1&showCalendars=0&src=dGMya2NtdmEyNW8zZXJtMmVmNWZicjZuZm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F09300" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>'
+        iframeSource = '<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23E4C441&ctz=America%2FNew_York&showPrint=0&title=Tennis%20Team%20Schedule&showTabs=0&showTz=1&showTitle=0&showNav=1&showDate=1&showCalendars=0&src=dGMya2NtdmEyNW8zZXJtMmVmNWZicjZuZm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F09300" style="border:solid 0px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>'
       } = props;
     return(
         <div className="body">
