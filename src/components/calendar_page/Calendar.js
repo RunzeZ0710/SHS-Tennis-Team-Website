ada26b3eaa09c670978e4107bd1efe47f14c57ce
TheBlueDestroyer <kjain24@students.shrewsbury.k12.ma.us>
2022-08-28 18:53:39 -0400

    calendar

--- src/components/calendar_page/Calendar.js
+++ src/components/calendar_page/Calendar.js
@@ -3,7 +3,7 @@ import "../calendar_page/Calendar.css"
 
 function Body(props) {
     const {
-        iframeSource = '<iframe frameborder="0" style="border-width:0;" id="cdr-calendar-iframe" width="900" height="500" src="https://www.chronoflocalendar.com/calendar/embed/5255/1777180349/"></iframe>'
+        iframeSource = '<iframe frameborder="0" style="border-width:0;" id="cdr-calendar-iframe" width="1200" height="600" src="https://www.chronoflocalendar.com/calendar/embed/5255/1777180349/"></iframe>'
       } = props;
     return(
         <div className="body">
